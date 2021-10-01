2f7ab14e0d5da1f0eb83899ba5472061b0e44b97
matias <matias@DESKTOP-95T80HL>
2021-10-01 17:29:34 -0300

    algo

--- PDA/src/pda/PDA.java
+++ PDA/src/pda/PDA.java
@@ -15,7 +15,7 @@ public class PDA {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("HOLI");
+        System.out.println("HOLa");
     }
     
 }
