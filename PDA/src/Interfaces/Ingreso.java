e300dc2bf8450498e0766ce2f5414b7a229e4799
matias <matias@DESKTOP-95T80HL>
2021-10-01 20:37:29 -0300

    algo

--- PDA/src/Interfaces/Ingreso.java
+++ PDA/src/Interfaces/Ingreso.java
@@ -5,6 +5,8 @@
  */
 package Interfaces;
 
+import java.awt.Color;
+
 /**
  *
  * @author matias
@@ -16,6 +18,7 @@ public class Ingreso extends javax.swing.JFrame {
      */
     public Ingreso() {
         initComponents();
+        ingresarAdmin.setBackground(Color.white);
     }
 
     /**
@@ -27,21 +30,75 @@ public class Ingreso extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel1 = new javax.swing.JPanel();
+        iconoAdmin = new javax.swing.JLabel();
+        ingresarAdmin = new javax.swing.JButton();
+        textoBienvenida1 = new javax.swing.JLabel();
+        bienvenido = new javax.swing.JLabel();
+        textoBienvenida2 = new javax.swing.JLabel();
+        ingresarCliente = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+        jPanel1.add(iconoAdmin, new org.netbeans.lib.awtextra.AbsoluteConstraints(710, 40, 60, 60));
+
+        ingresarAdmin.setFont(new java.awt.Font("Segoe UI Emoji", 0, 14)); // NOI18N
+        ingresarAdmin.setText("               Ingresar como administrador");
+        ingresarAdmin.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                ingresarAdminActionPerformed(evt);
+            }
+        });
+        jPanel1.add(ingresarAdmin, new org.netbeans.lib.awtextra.AbsoluteConstraints(710, 40, 270, 60));
+
+        textoBienvenida1.setFont(new java.awt.Font("Segoe UI", 2, 18)); // NOI18N
+        textoBienvenida1.setForeground(new java.awt.Color(255, 255, 255));
+        textoBienvenida1.setText("Para armar su carrito");
+        jPanel1.add(textoBienvenida1, new org.netbeans.lib.awtextra.AbsoluteConstraints(400, 320, 170, 50));
+
+        bienvenido.setFont(new java.awt.Font("Tahoma", 0, 48)); // NOI18N
+        bienvenido.setForeground(new java.awt.Color(255, 255, 255));
+        bienvenido.setText("Bienvenido(a)");
+        jPanel1.add(bienvenido, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 160, 320, 100));
+
+        textoBienvenida2.setFont(new java.awt.Font("Segoe UI", 2, 18)); // NOI18N
+        textoBienvenida2.setForeground(new java.awt.Color(255, 255, 255));
+        textoBienvenida2.setText("Presione para continuar");
+        jPanel1.add(textoBienvenida2, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 370, -1, -1));
+
+        ingresarCliente.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        ingresarCliente.setPreferredSize(new java.awt.Dimension(1000, 600));
+        ingresarCliente.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                ingresarClienteActionPerformed(evt);
+            }
+        });
+        jPanel1.add(ingresarCliente, new org.netbeans.lib.awtextra.AbsoluteConstraints(-30, 0, 1050, 600));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void ingresarClienteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ingresarClienteActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_ingresarClienteActionPerformed
+
+    private void ingresarAdminActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ingresarAdminActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_ingresarAdminActionPerformed
 
     /**
      * @param args the command line arguments
@@ -79,5 +136,12 @@ public class Ingreso extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel bienvenido;
+    private javax.swing.JLabel iconoAdmin;
+    private javax.swing.JButton ingresarAdmin;
+    private javax.swing.JButton ingresarCliente;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JLabel textoBienvenida1;
+    private javax.swing.JLabel textoBienvenida2;
     // End of variables declaration//GEN-END:variables
 }
